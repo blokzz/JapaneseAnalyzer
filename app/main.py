0cc692748b55055797f8004445f2810900f4976a
blokz <thebloxpl@gmail.com>
2026-08-19 17:19:42 +0200

    included stats

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from app.api import stats
 from fastapi import FastAPI
 from app.api import analyze, sentences, tokenize
 
@@ -7,6 +8,7 @@ from app.db.neo4j import lifespan
 
 
 app = FastAPI(title="Japanese RAG", version="0.1.0", lifespan=lifespan)
+app.include_router(stats.router, prefix="/stats", tags=["stats"])
 app.include_router(analyze.router, prefix="/analyze", tags=["analyze"])
 app.include_router(sentences.router, prefix="/sentences", tags=["sentences"])
 app.include_router(tokenize.router, prefix="/tokenize", tags=["tokenize"])
